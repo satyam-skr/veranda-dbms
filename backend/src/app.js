de409a73c255c77182dea5405b2bcc78dcfbac48
hars-lo <harshloya9@gmail.com>
2025-11-08 22:56:10 +0530

    first

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import userRoute from './routes/users.route.js'
 import pool from "./db/db.js";
 
-const app = express();h
+const app = express();
 
 app.use(express.json("limit:16kb"));
 app.use(express.urlencoded({extended:true}));
